19e0ae27ef69e0a433cbc956aa85da942d045932
Gil <ghopecs@gmail.com>
2023-11-09 16:54:13 -0500

    add wendler_func_2.py

---
+++ wendler_func_2.py
@@ -0,0 +1,44 @@
+def calculate_wendler_531(orms):
+    """
+    Calculate the Wendler 5/3/1 workout program based on the given 1RMs.
+
+    :param orms: A tuple containing the 1RM for squat, bench, deadlift, overhead press
+    :return: A dictionary with the workout plan for each week
+    """
+    workout_plan = {}
+    # Adjust the keys to match the naming convention you want, all uppercase
+    training_maxes = {lift: max * 0.9 for lift, max in zip(["SQUAT", "BENCH", "DEADLIFT", "OHP"], orms)}
+
+    # Define the rep schemes and percentages for each week
+    rep_schemes = {
+        "WEEK 1": [(5, 0.65), (5, 0.75), (5, 0.85)],
+        "WEEK 2": [(3, 0.70), (3, 0.80), (3, 0.90)],
+        "WEEK 3": [(5, 0.75), (3, 0.85), (1, 0.95)],
+        "WEEK 4": [(5, 0.40), (5, 0.50), (5, 0.60)]
+    }
+
+    # Calculate the weights for each set
+    for week, reps_and_percs in rep_schemes.items():
+        formatted_week = f'{week}:'  # Add colon here to match your desired output
+        workout_plan[formatted_week] = {}
+        for lift, training_max in training_maxes.items():
+            # Format the output with "Reps" and "lbs" as specified
+            workout_plan[formatted_week][lift] = [
+                (f'{reps} Reps', f'{round(training_max * perc / 5) * 5}lbs') for reps, perc in reps_and_percs
+            ]
+
+    return workout_plan
+
+
+
+# Test Call
+
+if __name__ == "__main__":
+    # Example 1RM inputs
+    my_1rms = (315, 225, 405, 135)  # Replace with your 1RM for squat, bench, deadlift, overhead press
+    workout_plan = calculate_wendler_531(my_1rms)
+    for week in workout_plan:
+        print(f"{week}")
+        for lift in workout_plan[week]:
+            print(f"{lift}: {workout_plan[week][lift]}")
+        print()  # Print a newline for better readability between weeks
