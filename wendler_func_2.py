7eadd37b4f7ab644cb321b6d2cfc6ca8075cced9
Gil <ghopecs@gmail.com>
2023-11-09 20:48:21 -0500

    remove output brackets

--- wendler_func_2.py
+++ wendler_func_2.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
             formatted_lift = lift.upper()
             print(f"{formatted_lift}: ")
             # Convert tuple to string and remove commas for required format
-            formatted_sets = [f"('{reps}, '{weight}')" for reps, weight in workout_plan[week][lift]]
-            print(f"[{', '.join(formatted_sets)}]")
+            # Convert tuple to string and format properly
+            formatted_sets = ', '.join(f"({reps}, {weight})" for reps, weight in workout_plan[week][lift])
+            print(formatted_sets)
         print()  # Print a newline for better readability between weeks
